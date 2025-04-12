d18f1352fa42dc17cfa899aa7c284c96d28ac25f
stefanpate <stefanpate94@gmail.com>
2025-04-12 10:30:05 -0500

    Uniquify ni rules. Test map_pathway and resolved_multi_mappings scripts with min, imt, mech rules on decarbs

---
+++ uniquify_ni_rules.py
@@ -0,0 +1,27 @@
+import hydra
+from omegaconf import DictConfig
+import pandas as pd
+from collections import defaultdict
+from pathlib import Path
+
+@hydra.main(version_base=None, config_path="conf", config_name="uniquify_ni_rules")
+def main(cfg: DictConfig):
+
+    ni_rules = pd.read_csv(cfg.src_file, sep="\t")
+    ni_sma_id = defaultdict(list)
+    for _, row in ni_rules.iterrows():
+        ni_sma_id[row["SMARTS"]].append(row["Name"])
+    
+    unique_rules = set(ni_rules["SMARTS"])
+    unique_rules = pd.DataFrame(
+        data=[(i, smarts, ni_sma_id[smarts]) for i, smarts in enumerate(unique_rules)],
+        columns=["id", "smarts", "ni_ids"]
+    )
+    unique_rules.to_csv(
+        f"{"_".join(Path(cfg.src_file).stem.split('_')[1:])}.csv",
+        sep=",",
+        index=False
+    )
+
+if __name__ == "__main__":
+    main()
