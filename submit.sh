440aa89d3b518f5978cd6bc67cc8c5667e5ec9c9
stefanpate <stefanpate94@gmail.com>
2025-05-18 16:08:05 -0500

    Mapped rcr to sprhea. Revived submit script

---
+++ submit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -A b1039
+#SBATCH -p b1039
+#SBATCH -N 1
+#SBATCH -n 50
+#SBATCH --mem=0
+#SBATCH -t 48:00:00
+#SBATCH --job-name="map_rxns"
+#SBATCH --output=/home/spn1560/enz-rxn-data/logs/out/%A
+#SBATCH --error=/home/spn1560/enz-rxn-data/logs/error/%A
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=stefan.pate@northwestern.edu
+
+# Args
+script=/home/spn1560/enz-rxn-data/map_pathway_level_reactions.py
+rxn=sprhea_240310_v3_mapped_no_subunits.json
+rule=rc_plus_1_rules.csv
+
+# Commands
+ulimit -c 0
+module purge
+source /home/spn1560/enz-rxn-data/.venv/bin/activate
+python $script rxn_file=$rxn rule_file=$rule
