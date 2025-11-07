efbf2f32fcc40d037f6ddf2084c4fbc37967470a
stefanpate <stefanpate94@gmail.com>
2025-11-07 07:23:25 -0600

    Copy in updated mechinferred rules

--- submit.sh
+++ submit.sh
@@ -4,21 +4,28 @@
 #SBATCH -N 1
 #SBATCH -n 50
 #SBATCH --mem=0
-#SBATCH -t 24:00:00
+#SBATCH -t 14:00:00
 #SBATCH --job-name="map"
-#SBATCH --output=/home/spn1560/enz-rxn-data/logs/out/%A
-#SBATCH --error=/home/spn1560/enz-rxn-data/logs/error/%A
+#SBATCH --output=/home/spn1560/enz-rxn-data/logs/out/%x_%A.out
+#SBATCH --error=/home/spn1560/enz-rxn-data/logs/error/%x_%A.err
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
+#SBATCH --array=0-4
 #SBATCH --mail-user=stefan.pate@northwestern.edu
 
 # Args
 script=/home/spn1560/enz-rxn-data/map_pathway_level_reactions.py
 rxn=known_reactions.parquet
-rule=rdchiral_rules.csv
+rule=(
+    mechinferred_dt_956_rules.csv
+    mechinferred_dt_056_rules.csv
+    mechinferred_dt_014_rules.csv
+    mechinferred_dt_006_rules.csv
+    mechinferred_dt_002_rules.csv
+)
 
 # Commands
 ulimit -c 0
 module purge
 source /home/spn1560/enz-rxn-data/.venv/bin/activate
-python $script rxn_file=$rxn rule_file=$rule
+python $script rxn_file=$rxn rule_file=${rule[$SLURM_ARRAY_TASK_ID]}
