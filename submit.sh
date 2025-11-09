73b9d590835e925571794263fdc5227ce459bbd7
stefanpate <stefanpate94@gmail.com>
2025-11-09 08:43:53 -0600

    Mappings for refreshed mechinferred rules

--- submit.sh
+++ submit.sh
@@ -4,24 +4,24 @@
 #SBATCH -N 1
 #SBATCH -n 50
 #SBATCH --mem=0
-#SBATCH -t 14:00:00
+#SBATCH -t 48:00:00
 #SBATCH --job-name="map"
-#SBATCH --output=/home/spn1560/enz-rxn-data/logs/out/%x_%A.out
-#SBATCH --error=/home/spn1560/enz-rxn-data/logs/error/%x_%A.err
+#SBATCH --output=/home/spn1560/enz-rxn-data/logs/out/%x_%A_%a.out
+#SBATCH --error=/home/spn1560/enz-rxn-data/logs/error/%x_%A_%a.err
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --array=0-4
+#SBATCH --array=0
 #SBATCH --mail-user=stefan.pate@northwestern.edu
 
 # Args
 script=/home/spn1560/enz-rxn-data/map_pathway_level_reactions.py
 rxn=known_reactions.parquet
 rule=(
-    mechinferred_dt_956_rules.csv
-    mechinferred_dt_056_rules.csv
+    # mechinferred_dt_956_rules.csv
+    # mechinferred_dt_056_rules.csv
     mechinferred_dt_014_rules.csv
-    mechinferred_dt_006_rules.csv
-    mechinferred_dt_002_rules.csv
+    # mechinferred_dt_006_rules.csv
+    # mechinferred_dt_002_rules.csv
 )
 
 # Commands
